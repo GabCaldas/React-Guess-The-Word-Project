c84dd9b96d8b0c24688d0cab21d5e728a7bd8259
GabCaldas <100331767+GabCaldas@users.noreply.github.com>
2023-02-07 20:47:05 -0300

    second

--- components/StartingScreen.jsx
+++ components/StartingScreen.jsx
@@ -6,7 +6,7 @@ const StartingScreen = () => {
         <div className='StartScreen'>
             <h1 className='title'>Guess the <strong>WORD</strong></h1>
             <p>Click on the botton below to start</p>
-            <button class="button-49" role="button">START</button>
+            <button class="button-49" role="button">BEGIN</button>
         </div>
     )
 }
