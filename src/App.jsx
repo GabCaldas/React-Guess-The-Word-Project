a1d319ebaa329fdc2c1f5493a2faa20d03ea4543
GabCaldas <100331767+GabCaldas@users.noreply.github.com>
2023-02-09 20:53:48 -0300

    fourth

--- src/App.jsx
+++ src/App.jsx
@@ -22,9 +22,44 @@ function App() {
   const [gameStage, setGameStage] = useState(stages[0].name);
   const [words] = useState(wordsList)
 
+  const [pickedWord, setPickedWord] = useState("")
+  const [pickedCategory, setPickedCategory] = useState("")
+  const [letters, setLetters] = useState([])
+
+
+
+  const pickWordAndCategory = () => {
+    // PICK A RANDOM CATEGORY 
+    const categories = Object.keys(words);
+    const category =
+      categories[Math.floor(Math.random() * Object.keys(categories).length)];
+    console.log(category)
+
+    // PICK A RANDOM WORD
+    const word = words[category][Math.floor(Math.random() * words[category].length)]
+    console.log(word)
+
+    return { word, category }
+  }
+
+
   //START THE GAME
   const startGame = () => {
+    // PICK WORD AND CATEGORY
+    const { word, category } = pickWordAndCategory()
+
+    //CREATE AN ARRAY OF LETTERS
+    let wordLetters = word.split("")
+
+    wordLetters = wordLetters.map((l) => l.toLowerCase())
     setGameStage(stages[1].name)
+
+
+    //FILL STATES
+    setPickedWord(word)
+    setPickedCategory(category)
+    setLetters(letters)
+
   }
 
 
